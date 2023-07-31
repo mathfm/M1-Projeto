fa6530813c53ff9a12f00941f6204bcb22fa62ff
mathfm <fernandomath.mds@gmail.com>
2023-07-31 15:32:17 -0300

    nome de classes e ids trocados

--- script/script.js
+++ script/script.js
@@ -1,6 +1,43 @@
-let btnResposta = document.querySelectorAll("#questao-1 .resposta")
-let questaoUm = document.getElementById("questao-1")
-let questaoDois = document.getElementById("questao-2")
+let btnResposta = document.querySelectorAll("#questao-0 .btn-pergunta")
+let questaoUm = document.getElementById("questao-0")
+let questaoDois = document.getElementById("questao-1")
+let perguntasTotal = document.querySelectorAll("#questao-0, #questao-1, #questao-2, #questao-3, #questao-4, #questao-5")
+let btnGeral = document.querySelectorAll(".btn-pergunta")
+
+
+//let auxBtn = 0;
+
+
+
+
+
+function alternarDiv(exibirQuestao, ocultarQuestao) {
+    let questaoAtual = document.getElementById("questao-" + exibirQuestao)
+    let proximaQuestao = document.getElementById("questao-" + ocultarQuestao);
+
+    questaoAtual.classList.remove("desativar");
+    proximaQuestao.classList.add("desativar")
+}
+
+
+
+
+
+
+
+
+
+/*
+for (let i = 0; i < perguntasTotal.length; i++) {
+    
+    for (let j = 0; j < btnGeral.length/perguntasTotal.length; j++) {
+        
+        
+
+    }
+    
+}
+
 
 
 for (let i = 0; i < btnResposta.length; i++) {
@@ -11,6 +48,24 @@ for (let i = 0; i < btnResposta.length; i++) {
     }
     
 )}
+
+
+for (let i = 0; i < perguntasTotal.length; i++) {
+    for (let j = 0; j < btnGeral.length; j++) {
+        auxBtn++;
+        if (j < 4) {
+            if (i == 0) {
+                btnGeral[j].addEventListener("click", () => {
+                    perguntasTotal[i].classList.toggle("desativar")
+                    perguntasTotal[i+1].classList.toggle("desativar")
+                })
+            } 
+        }
+    }
+    
+}
+*/
+
 
 
 
