e49efd8f16d69b497879e4a17aed80d251f1eaa0
mathfm <fernandomath.mds@gmail.com>
2023-07-31 23:48:22 -0300

    logica de alternar perguntas

--- script/script.js
+++ script/script.js
@@ -1,11 +1,9 @@
 let btnResposta = document.querySelectorAll("#questao-0 .btn-pergunta")
-let questaoUm = document.getElementById("questao-0")
-let questaoDois = document.getElementById("questao-1")
 let perguntasTotal = document.querySelectorAll("#questao-0, #questao-1, #questao-2, #questao-3, #questao-4, #questao-5")
 let btnGeral = document.querySelectorAll(".btn-pergunta")
 
 
-//let auxBtn = 0;
+
 
 
 
@@ -15,9 +13,10 @@ function alternarDiv(exibirQuestao, ocultarQuestao) {
     let questaoAtual = document.getElementById("questao-" + exibirQuestao)
     let proximaQuestao = document.getElementById("questao-" + ocultarQuestao);
 
-    questaoAtual.classList.remove("desativar");
-    proximaQuestao.classList.add("desativar")
+    questaoAtual.classList.toggle("desativar");
+    proximaQuestao.classList.toggle("desativar")
 }
+
 
 function abrirAjuda(){
     const ajuda = document.getElementById ('janela-ajuda')
@@ -33,6 +32,22 @@ function abrirAjuda(){
 
 
 
+
+for (let i = 0; i < perguntasTotal.length; i++) {
+    const btnTodos = document.querySelectorAll(`.btn-p${i}`)
+    for (let j = 0; j < btnTodos.length; j++) {
+
+        if (i < perguntasTotal.length - 1) {
+            btnTodos[j].addEventListener("click", () => {
+                alternarDiv(i, i + 1)
+            }) 
+        } else {
+            btnTodos[j].addEventListener("click", () => { 
+                console.log("fim");
+            })
+        }
+    }
+}
 
 
 
